6b22db8d508cf9dc1fa63637f1068dc323172db9
asks47 <asks.aks@gmail.com>
2017-09-25 14:52:47 +0530

    Delete EmailStartUp.sh

--- ShellScripting/Automate/EmailStartUp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssmtp asharma@snapsystems.in adubey@snapsystems.in vshinde@snapsystems.in ajain@snapsystems.in nmarkam@snapsystems.in < /home/ss4/Documents/Ashwini_folder/ShellScripting/Automate/msg.txt;
